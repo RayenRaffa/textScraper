30cdca915cb58deca351249c0bd08753cdff6234
raffa <raffa.rayen@gmail.com>
2019-11-03 14:43:04 +0100

    updated postProcess.py for product aggregation

--- postProcess.py
+++ postProcess.py
@@ -8,28 +8,29 @@ from pandas import ExcelFile
 
 def PostProcess(data_dir='./out'):
 
-    log_dir = './Logs/'
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    if(log_dir):
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        log_file = open(log_dir + "postProcess.log", "a")
+        old_stdout = sys.stdout
+        sys.stdout = log_file
 
     print("Scanning files ...")
 
-    old_stdout = sys.stdout
-    log_file = open(log_dir+"postProcess.log","w")
-    sys.stdout = log_file
 
     agg_data_df = pd.DataFrame(columns=['Name', 'URL', 'Phone', 'Address', 'Category','Industry'])
     for root, dirs, files in os.walk(data_dir):
-            for name in files:
+        for name in files:
+            if name == re.compile("products"):
                 file_name = os.path.join(root,name)
                 subcat_df = pd.read_excel(file_name)
                 agg_data_df = agg_data_df.append(subcat_df, ignore_index=True)
 
     agg_data_df.sort_values('Name',inplace=True)
     agg_data_df.drop_duplicates('URL',inplace=True)
-    print(f"Found {len(agg_data_df.index)} distinct URLs")
+    print(f"Found {len(agg_data_df.index)} distinct product URLs")
 
-    writer = ExcelWriter(data_dir+'sellers.xlsx')
+    writer = ExcelWriter(data_dir+'all_products.xlsx')
     d = {'Name':agg_data_df['Name'],
             'URL':agg_data_df['URL'],
             'Phone':agg_data_df['Phone'],
@@ -43,16 +44,17 @@ def PostProcess(data_dir='./out'):
     writer.save()
     writer.close()
 
-    sample_writer = ExcelWriter(data_dir+'sampleSellers.xlsx')
-    sample_df = df.iloc[0:500,:]
-    sample_df.to_excel(sample_writer)
-    sample_writer.save()
-    sample_writer.close()
-    sys.stdout = old_stdout
-    log_file.close()
+    # sample_writer = ExcelWriter(data_dir+'sampleSellers.xlsx')
+    # sample_df = df.iloc[0:500,:]
+    # sample_df.to_excel(sample_writer)
+    # sample_writer.save()
+    # sample_writer.close()
 
+    if(log_dir):
+        sys.stdout = old_stdout
+        log_file.close()
 
-    print(f"Found {len(agg_data_df.index)} distinct URLs")
+    print(f"Found {len(agg_data_df.index)} distinct product URLs")
     print("PostProcess : DONE.")
     
     return 0
