1a51963abba7af461aa165b930b5fce480fac060
raffa <raffa.rayen@gmail.com>
2019-11-03 18:10:33 +0100

    updated error handling for http requests

--- extractVendors.py
+++ extractVendors.py
@@ -23,66 +23,70 @@ def ExtractVendors(product,out_dir='./out',log_dir=None):
     prod_indus  = product.Industry
 
     # Query the URL for its html
-    prod_page = urllib.request.urlopen(prod_url)
+    try:
+        prod_page = urllib.request.urlopen(prod_url)
+    except Exception as e:
+        print(f"Error fetching product URL\n{e}\nSkipping ..")
+        prod_page = None
+    if(prod_page):
+        # Parsing the URL page into BeautifulSoup format
+        prod_soup = BeautifulSoup(prod_page, 'html.parser')
 
-    # Parsing the URL page into BeautifulSoup format
-    prod_soup = BeautifulSoup(prod_page, 'html.parser')
+        # Extracting vendor box from the webpage
+        vendor_list = prod_soup.find('ul', attrs={'id': 'm'})
+        vendors_box = vendor_list.find_all('li', attrs={'id': re.compile('LST')})
+        vendors = pd.DataFrame(columns={'Name','URL','Phone','Address','Category','Industry'})
 
-    # Extracting vendor box from the webpage
-    vendor_list = prod_soup.find('ul', attrs={'id': 'm'})
-    vendors_box = vendor_list.find_all('li', attrs={'id': re.compile('LST')})
-    vendors = pd.DataFrame(columns={'Name','URL','Phone','Address','Category','Industry'})
-
-    for vendor_box in vendors_box:
-        try:
-            vendor_info_box = vendor_box.find(
-                'div', attrs={'class': 'r-cl b-gry'})
-            vendor_info = vendor_info_box.find('a', href=True)
-            vendor_name = vendor_info.getText().strip()
-            vendor_url  = vendor_info['href']
-            # print(vendor_url.text)
-            # print(vendor_url['href'])
+        for vendor_box in vendors_box:
             try:
-                vendor_number = vendor_info_box.find('div', attrs={'id': re.compile('mobenq')}).getText()
-                vendor_number = vendor_number.strip('cCaAlL')
-                # print(vendor_number)
-                # print(vendor_number.text)
+                vendor_info_box = vendor_box.find(
+                    'div', attrs={'class': 'r-cl b-gry'})
+                vendor_info = vendor_info_box.find('a', href=True)
+                vendor_name = vendor_info.getText().strip()
+                vendor_url  = vendor_info['href']
+                # print(vendor_url.text)
+                # print(vendor_url['href'])
+                try:
+                    vendor_number = vendor_info_box.find('div', attrs={'id': re.compile('mobenq')}).getText()
+                    vendor_number = vendor_number.strip('cCaAlL')
+                    # print(vendor_number)
+                    # print(vendor_number.text)
+                except Exception as e:
+                    print(f"!! !! Error fetching vendor phone\n{e}\n")
+                    vendor_number = 'Not Found'
+                try:
+                    vendor_address = vendor_info_box.find('p', attrs={'class': 'sm clg'}).getText().strip()
+                    # print(vendor_address.text)
+                except Exception as e:
+                    print(f"!! Error fetching vendor address\n{e}\n")
+                    vendor_address = 'Not Found'
+                    
+                vendors = vendors.append({'Name': vendor_name,
+                                          'URL': vendor_url,
+                                          'Phone': vendor_number,
+                                          'Address': vendor_address,
+                                          'Category': prod_cat,
+                                          'Industry': prod_indus},
+                                         ignore_index=True, sort=False)
             except Exception as e:
-                print(f"!! !! Error fetching vendor phone\n{e}\n")
-                vendor_number = 'Not Found'
-            try:
-                vendor_address = vendor_info_box.find('p', attrs={'class': 'sm clg'}).getText().strip()
-                # print(vendor_address.text)
-            except Exception as e:
-                print(f"!! Error fetching vendor address\n{e}\n")
-                vendor_address = 'Not Found'
-
-            vendors = vendors.append({'Name': vendor_name,
-                                      'URL': vendor_url,
-                                      'Phone': vendor_number,
-                                      'Address': vendor_address,
-                                      'Category': prod_cat,
-                                      'Industry': prod_indus},
-                                     ignore_index=True, sort=False)
-        except Exception as e:
-            print(f"\n!!!!!!!!!!!!!  Error fetching vendor data\n{e} .. SKIPPING ...\n")
-
-    if (len(vendors.index) > 0):
-        vendors.sort_values('Name', inplace=True)
-        vendors.drop_duplicates('URL', inplace=True)
-        print(f'\nFound : {len(vendors.index)}  vendors in {prod_name} : creating file ...')
-        prod_out_dir = out_dir + '/' + prod_indus + '/' + prod_cat + '/' + prod_subCat
-        vendors_file = prod_out_dir + '/vendorsOf_' + prod_name + '.xlsx'
-        if not os.path.exists(prod_out_dir):
-            os.makedirs(prod_out_dir)
-        writer = ExcelWriter(vendors_file) # TO DO : adapt script to write multiple sheets per file, one industry per file
-        vendors.to_excel(writer, index=False)
-        writer.save()
-        writer.close()
-        print(f"Saved vendors of {prod_name} at:\n{vendors_file}\n")
-    else:
-        print(f"\nNo vendors found at {prod_url} : SKIPPING ..\n")
-
+                print(f"\n!!!!!!!!!!!!!  Error fetching vendor data\n{e} .. SKIPPING ...\n")
+        
+        if (len(vendors.index) > 0):
+            vendors.sort_values('Name', inplace=True)
+            vendors.drop_duplicates('URL', inplace=True)
+            print(f'\nFound : {len(vendors.index)}  vendors in {prod_name} : creating file ...')
+            prod_out_dir = out_dir + '/' + prod_indus + '/' + prod_cat + '/' + prod_subCat
+            vendors_file = prod_out_dir + '/vendorsOf_' + prod_name + '.xlsx'
+            if not os.path.exists(prod_out_dir):
+                os.makedirs(prod_out_dir)
+            writer = ExcelWriter(vendors_file) # TO DO : adapt script to write multiple sheets per file, one industry per file
+            vendors.to_excel(writer, index=False)
+            writer.save()
+            writer.close()
+            print(f"Saved vendors of {prod_name} at:\n{vendors_file}\n")
+        else:
+            print(f"\nNo vendors found at {prod_url} : SKIPPING ..\n")
+        
     if(log_dir):
         sys.stdout = old_stdout
         log_file.close()
